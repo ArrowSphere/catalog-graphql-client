154c868af625452996cfde9f5c7ead07df3a4a67
Pierre Feyssaguet <pfeyssaguet@gmail.com>
2021-02-04 09:54:59 +0100

    Moved CatalogGraphQLClient::prepareInput() into a new class InputPreparator to get it under tests

---
+++ src/Helpers/InputPreparator.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace ArrowSphere\CatalogGraphQLClient\Helpers;
+
+/**
+ * Class InputPreparator
+ */
+class InputPreparator
+{
+    /**
+     * @param array $data
+     * @return string
+     */
+    public function prepareInput(array $data): string
+    {
+        // This is a quick implementation of a kind of json_encode but without double quotes on the keys (like a JS object)
+        // Because this lib wants it like that for RawObject...
+        // I'm sure there are bugs in it but for my use cases it works... if you run into bugs please add tests in InputPreparatorTest
+        $res = [];
+
+        $sequential = array_is_list($data);
+
+        foreach ($data as $name => $value) {
+            $prefix = $sequential ? '' : $name . ': ';
+
+            if (is_array($value)) {
+                $res[] = $prefix . $this->prepareInput($value);
+            } else {
+                if (is_bool($value)) {
+                    $value = $value ? 'true' : 'false';
+                }
+                $res[] = $prefix . (is_string($value) ? '"' . $value . '"' : $value);
+            }
+        }
+
+        if ($sequential) {
+            return '[' . implode(', ', $res) . ']';
+        }
+
+        return '{' . implode(', ', $res) . '}';
+    }
+}
---
+++ tests/Helpers/InputPreparatorTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace ArrowSphere\CatalogGraphQLClient\Tests\Helpers;
+
+use ArrowSphere\CatalogGraphQLClient\Helpers\InputPreparator;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class InputPreparatorTest
+ */
+class InputPreparatorTest extends TestCase
+{
+    public function providerPrepareInput(): array
+    {
+        return [
+            'empty array' => [
+                'data'     => [],
+                'expected' => '[]',
+            ],
+            'pagination'  => [
+                'data'     => [
+                    'page'    => 1,
+                    'perPage' => 50,
+                ],
+                'expected' => '{page: 1, perPage: 50}',
+            ],
+            'searchBody'  => [
+                'data' => [
+                    'filters' => [
+                        [
+                            'name'  => 'field1',
+                            'value' => 'value1',
+                        ],
+                        [
+                            'name'  => 'field2',
+                            'value' => [
+                                'value2.1',
+                                'value2.2',
+                            ],
+                        ],
+                    ],
+                ],
+                'expected' => '{filters: [{name: "field1", value: "value1"}, {name: "field2", value: ["value2.1", "value2.2"]}]}',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider providerPrepareInput
+     * @param array $data
+     * @param string $expected
+     */
+    public function testPrepareInput(array $data, string $expected): void
+    {
+        $inputPreparator = new InputPreparator();
+
+        self::assertSame($expected, $inputPreparator->prepareInput($data));
+    }
+}
