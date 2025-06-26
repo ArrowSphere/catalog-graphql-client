ee4d50c4ff9da4123abd65bbcab76191002b5275
Maher Landolsi <maher.landolsi@arrow.com>
2025-06-26 18:27:04 +0200

    add type to AttributesParameters

--- src/Types/AttributesParameters.php
+++ src/Types/AttributesParameters.php
@@ -8,9 +8,11 @@ namespace ArrowSphere\CatalogGraphQLClient\Types;
  * @method string getName()
  * @method string getLabel()
  * @method int getPosition()
+ * @method string getType()
  * @method self setName(string $name)
  * @method self setLabel(string $label)
  * @method self setPosition(int $position)
+ * @method self setType(string $type)
  */
 class AttributesParameters extends AbstractType
 {
@@ -20,9 +22,16 @@ class AttributesParameters extends AbstractType
 
     public const POSITION = 'position';
 
+    public const TYPE = 'type';
+
     protected const MAPPING = [
         self::NAME  => self::TYPE_STRING,
         self::LABEL => self::TYPE_STRING,
         self::POSITION => self::TYPE_INT,
+        self::TYPE => self::TYPE_STRING,
     ];
+
+    public const ATTRIBUTE_TYPE_STANDARD = 'STANDARD';
+    public const ATTRIBUTE_TYPE_INTERNAL = 'INTERNAL';
+    public const ATTRIBUTE_TYPE_READ_ONLY = 'READ_ONLY';
 }
